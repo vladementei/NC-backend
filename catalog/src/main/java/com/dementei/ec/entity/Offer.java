4079c7ef0baa65f6d1180f9a95afe8410207aa92
Vlad Dementei <vladementei@gmail.com>
2019-11-18 18:05:44 +0200

    feat: catalog entities

---
+++ catalog/src/main/java/com/dementei/ec/entity/Offer.java
@@ -0,0 +1,52 @@
+package com.dementei.ec.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "offers")
+public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "offer_id", nullable = false, unique = true)
+    private long id;
+
+    @Column(nullable = false)
+    private String title = "";
+
+    @Column(nullable = false)
+    private String description = "";
+
+    @OneToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "price_id")
+    //optional false - this price must be in the 'prices' table
+    private Price price;
+
+    @ManyToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    //optional false - this category must be in the 'categories' table
+    private Category category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return getId() == offer.getId() &&
+                Objects.equals(getTitle(), offer.getTitle()) &&
+                Objects.equals(getDescription(), offer.getDescription()) &&
+                Objects.equals(getPrice(), offer.getPrice()) &&
+                Objects.equals(getCategory(), offer.getCategory());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getDescription(), getPrice(), getCategory());
+    }
+}
---
+++ catalog/src/main/java/com/dementei/ec/entity/Price.java
@@ -0,0 +1,40 @@
+package com.dementei.ec.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "prices")
+public class Price {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "price_id", nullable = false, unique = true)
+    private long id;
+
+    @Column(nullable = false)
+    double price = 0.0;
+
+    @OneToOne(mappedBy = "price")
+    private Offer offer;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return getId() == price1.getId() &&
+                Double.compare(price1.getPrice(), getPrice()) == 0 &&
+                Objects.equals(getOffer(), price1.getOffer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPrice(), getOffer());
+    }
+}
