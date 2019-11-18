265f0e41df9f4fc64ec8a32281cf19934832e725
Vlad Dementei <vladementei@gmail.com>
2019-11-18 18:45:02 +0200

    feat: catalog application

---
+++ catalog/src/main/java/com/dementei/ec/CatalogApplication.java
@@ -0,0 +1,14 @@
+package com.dementei.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication
+public class CatalogApplication {
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(CatalogApplication.class, args);
+    }
+}
