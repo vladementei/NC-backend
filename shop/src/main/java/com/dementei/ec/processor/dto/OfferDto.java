560ae0d74a595ec235e03f305577ecced0a20ef6
Vlad Dementei <vladementei@gmail.com>
2019-11-26 11:14:44 +0200

    feat: shop Processor Dto

---
+++ shop/src/main/java/com/dementei/ec/processor/dto/OfferDto.java
@@ -0,0 +1,22 @@
+package com.dementei.ec.processor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class OfferDto {
+    private long id = 0;
+    private String title = "";
+    private String description = "";
+    private double price = 0.0;
+    private CategoryInfoDto category = new CategoryInfoDto(0, "");
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CategoryInfoDto{
+        private long id;
+        private String category;
+    }
+}
---
+++ shop/src/main/java/com/dementei/ec/processor/dto/OrderOffersDto.java
@@ -0,0 +1,19 @@
+package com.dementei.ec.processor.dto;
+
+import com.dementei.ec.inventory.entity.OrderStatus;
+import com.dementei.ec.inventory.entity.PaymentStatus;
+import lombok.Data;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class OrderOffersDto {
+    private long id = 0;
+    private Set<Long> offers = new HashSet<>();
+    private String email = "";
+    private Calendar timeStamp = Calendar.getInstance();
+    private String paymentStatus = PaymentStatus.NOT_PAID.name();
+    private String orderStatus = OrderStatus.IN_PROCESS.name();
+}
